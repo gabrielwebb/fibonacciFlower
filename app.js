8a6d9f82b3daf39f6f972d57f284f9bf91eae901
Gabriel Webb <gabrielwebb90@gmail.com>
2020-09-15 16:29:40 -0400

    initial commit first commit. made changes to if statement value, angle and ctx.arc values.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let hue = Math.random() * 360;
 let number = 0;
 let scale = 10;
 function drawFlower() {
-  let angle = number * 2.2;
+  let angle = number * 2.52;
   let radius = scale * Math.sqrt(number);
   let positionX = radius * Math.sin(angle) + canvas.width / 2;
   let positionY = radius * Math.cos(angle) + canvas.height / 2;
@@ -15,7 +15,7 @@ function drawFlower() {
   ctx.strokeStyle = "black";
   ctx.lineWidth = 4;
   ctx.beginPath();
-  ctx.arc(positionX, positionY, 4.8, 0, Math.PI * 2);
+  ctx.arc(positionX, positionY, 4.78, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
@@ -24,7 +24,7 @@ function drawFlower() {
 }
 function animate() {
   drawFlower();
-  if (number > 500) return;
+  if (number > 720) return;
   requestAnimationFrame(animate);
 }
 animate();
